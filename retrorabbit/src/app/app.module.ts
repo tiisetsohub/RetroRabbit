90ccc6a51f9f75076438b08b018453f3645728ee
tiisetso <tiimojalefa@gmail.com>
2022-04-23 13:16:37 +0200

    header

--- retrorabbit/src/app/app.module.ts
+++ retrorabbit/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { Header2Component } from './header2/header2.component';
+import { HomeComponent } from './home/home.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    Header2Component
+    Header2Component,
+    HomeComponent,
+    TabComponent
   ],
   imports: [
     BrowserModule,
